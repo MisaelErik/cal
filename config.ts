87e168b0e2322ec57493776a9138071a605b52c1
MisaelErik <emquinonezm@unac.edu.pe>
2025-07-28 17:30:27 -0500

    Update config.ts

--- config.ts
+++ config.ts
@@ -3,9 +3,9 @@
 // It is strongly recommended to use environment variables on your hosting provider.
 
 export const API_KEYS = {
-    API_KEY: 'AIzaSyCwtmIRByCVhWDx4i0Q3QbaOFmz_...',
-    OPENROUTER_KIMI_API_KEY: 'sk-or-v1-efcd95c576f4fed2612daa85...',
-    OPENROUTER_MISTRAL_API_KEY: 'sk-or-v1-4efafa12ae3dc4cd797510c9...',
-    OPENROUTER_GEO_API_KEY: 'sk-or-v1-5d59f651edf6c780c68216a2...',
-    OPENROUTER_DEEPSEEK_API_KEY: 'sk-or-v1-b99d74d2bb9278f9334b7a41...',
+    API_KEY: 'AIzaSyCWtmIRByCVhWDx4i0Q3QbaOFmz_1cpcLc',
+    OPENROUTER_KIMI_API_KEY: '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',
+    OPENROUTER_MISTRAL_API_KEY: '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',
+    OPENROUTER_GEO_API_KEY: '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',
+    OPENROUTER_DEEPSEEK_API_KEY: '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',
 };
